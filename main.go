fcb6fadcdf15dccbfde9d40171ea14b2af951616
Yuya Kusakabe <y-kusakabe@bbsakura.net>
2020-04-08 15:02:18 +0900

    Add xss.html

--- main.go
+++ main.go
@@ -24,10 +24,19 @@ func main() {
 	h3 := func(w http.ResponseWriter, _ *http.Request) {
 		io.WriteString(w, verificationCode)
 	}
+	xss := func(w http.ResponseWriter, req *http.Request) {
+		query := req.URL.Query()
+		if q, ok := query["q"]; ok {
+			io.WriteString(w, fmt.Sprintf("<html><div>%s</div></html>", q))
+		} else {
+			io.WriteString(w, "<html>no params</html>")
+		}
+	}
 
 	http.HandleFunc("/", h1)
 	http.HandleFunc("/endpoint", h2)
 	http.HandleFunc(fmt.Sprintf("/vaddy-%s.html", verificationCode), h3)
+	http.HandleFunc("/xss.html", xss)
 
 	log.Fatal(http.ListenAndServe(listenAddr, nil))
 }
